27c7b31b63989cc6b32b1e86c4201a6c22200724
JacobPCM <jacobd@postcardmania.com>
2024-04-09 13:10:31 -0400

    Get icon images to work in html if possible

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './style.css';
+import sun from './images/sun.svg';
+
+const body = document.querySelector('body');
+const imageSun = document.createElement('img');
+imageSun.src = sun;
+body.appendChild(imageSun);
 
 console.log('test');

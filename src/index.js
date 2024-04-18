0b85b53c047b86cded8a06f515213293c446b20b
JacobPCM <jacobd@postcardmania.com>
2024-04-18 16:53:22 -0400

    Configure overlay for Adding a To-do

--- src/index.js
+++ src/index.js
@@ -2,17 +2,38 @@ import './style.css';
 import { renderAddTasks, categorySetup } from './sidebarCat.js';
 import { allTodos } from './todoGenerator.js';
 import { renderAllTodos } from './alltodosDOM.js';
+
+function clearContent(){
+    const content = document.querySelector('.content');
+    content.innerHTML = '';
+}
+
+function addButtonSetup(){
+    const add = document.querySelector('.addButton');
+    add.addEventListener('click', () => {
+        const title = document.querySelector('#title');
+        const description = document.querySelector('#description');
+        const dueDate = document.querySelector('#duedate');
+        const priority = document.querySelector('#priority');
+        const project = document.querySelector('#project');
+
+        tasks.addTodo(title.value, description.value, dueDate.value, priority.value, project.value)
+        clearContent();
+        renderAllTodos(tasks.getArrayTodos());
+    })
+};
+
+addButtonSetup();
 renderAddTasks();
 categorySetup();
 
 const tasks = allTodos();
 tasks.addTodo('Groceries', 'get them groceries', 'Today', 'Important', 'none');
 tasks.addTodo('Groceries', 'get them groceries', 'Today', 'Important', 'none');
-tasks.addTodo('Groceries', 'get them groceries', 'Today', 'Important', 'none');
-tasks.addTodo('Groceries', 'get them groceries', 'Today', 'Important', 'none');
-tasks.addTodo('Groceries', 'get them groceries', 'Today', 'Important', 'none');
-tasks.addTodo('Groceries', 'get them groceries', 'Today', 'Important', 'none');
+
+
 console.log(tasks.getArrayTodos());
+
 
 renderAllTodos(tasks.getArrayTodos());
 
